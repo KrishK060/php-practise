725f61b2c0fea127274ee1cd629711eebc337a3d
Krishk060 <krishkalaria0601@gmail.com>
2025-03-13 14:50:37 +0530

    implemented php variable,echo,strings and its function ,numbers

--- index.php
+++ index.php
@@ -1,10 +1,92 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
+<html>
+<body> -->
+
+<!-- <?php
+#echo "My first PHP script!";
+?>  -->
+
+<!-- </body>
+</html> -->
 <html>
 <body>
 
-<?php
-echo "My first PHP script!";
-?> 
+<!-- <?php
+$x = 5; // global scope
+ 
+function myTest() {
+    $x = 50;
+  // using x inside this function will generate an error
+  echo "<p>Variable x inside function is: $x</p>";
+} 
+myTest();
 
-</body>
+echo "<p>Variable x outside function is: $x</p>";
+?> -->
+
+<?php
+// $x = 5;
+// //var_dump($x);
+// $y = 10;
+
+// function myTest1() {
+//   $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
+// } 
+
+// myTest1();
+// echo $y;
+
+// class Car{
+//     public $color;
+//     public $model;
+//     public function __construct($color,$model){
+//         $this->color = $color;
+//         $this->model = $model;  
+//     }
+//     public function message() {
+//         return "My car is a " . $this->color . " " . $this->model . "!";
+//       }
+// }
+// $myCar = new Car("red", "Volvo");
+// var_dump($myCar);
+
+$x = "abc add";
+$Y="ASDC";
+//echo "the length of string:". strlen($x);
+
+//echo "the word count is :".str_word_count($x);
+
+//echo strpos($x,"add")
+
+//echo strtoupper($x);
+
+//echo strtolower( $Y );
+
+//echo str_replace("add", "Dolly", $x);
+//echo $z = str_replace("add", "Dolly", $x);
+
+// echo strrev($x);
+
+// $x = "Hello World!";
+// echo substr($x, 6, 5);
+
+//  $x = "Hello World!";
+//  echo substr($x, 6,);
+
+// $x = "Hello World!";
+// echo substr($x, -5, 3);
+
+// $x = 5985;
+// var_dump(is_int($x));
+
+// $x = acos(8);
+// var_dump($x);
+
+$x = 23465.768;
+$int_cast = (int)$x;
+echo $int_cast;
+?>
+
+0</body>
 </html>
+
