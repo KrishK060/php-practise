5d2d06f86e7224f27b37cbf0c0a58395389e6ce8
Krishk060 <krishkalaria0601@gmail.com>
2025-03-17 13:32:44 +0530

    created a condition & loops.php implemented types of loop and constants

---
+++ condition & loops.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $a = 5;
+
+    // if ($a == 2 || $a == 3 || $a == 4 || $a == 5 || $a == 6 || $a == 7) {
+    //   echo "$a is a number between 2 and 7";
+    // }
+   
+    // $t = date("H");
+
+    // if ($t < "10") {
+    //   echo "Have a good day!";
+    // } else {
+    //   echo "Have a good night!";
+    // }
+   
+    $a=16;
+    // $b = $a<10?"hello":"goood";
+    // echo $b;
+   
+//    switch ($a) {
+//     case 13:
+//         echo "this is 13";
+//         break;
+//     case 15:
+//         echo "THIS IS 15";
+// break;
+// case 16:
+//     echo "this si 16";
+//     break;
+//    }
+   
+//    while($a<20){
+//     $a++;
+//     if($a==18) break;  
+//     echo $a;
+// }
+   
+//  do{
+//     echo $a;
+//     $a++;
+//  } while($a<6);  
+   
+// for( $i= 0; $i< 10; $i++ ){
+//     if( $i%2== 0)continue;
+//     echo $i;
+// }
+
+// $member = array("1"=>"one","2"=>"two","3"=>"three");
+// foreach($member as $a=> $b){
+//     echo " ". $a ." ". $b ."  <br>";
+// }
+
+// $color = array("red","blue","green","yellow","orange");
+// foreach($color as $c){
+//     if($c == "green"){
+//         $c = "pink";}
+// }
+// var_dump($color);
+
+
+?>
+</body>
+</html>
