63db4dbb895332599dd399469c03bf492f3c1fb8
Krishk060 <krishkalaria0601@gmail.com>
2025-03-17 18:46:09 +0530

    created a arrays.php file

---
+++ arrays.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    <?php
+    // $color = array("blue","black","red");
+    // echo count($color);
+
+    // array_push($color,"pink");
+    // var_dump($color);
+
+
+    // $car = array("brand"=>"Ford", "model"=>model(), "year"=>year());
+    
+    // echo $car["brand"]
+
+    // $car["brand"] = "suzuki";
+    // var_dump($car);
+
+    // function myFunction() {
+    //     echo "I come from a function!";
+    //   }
+    //   function myage() {
+    //     echo "I come from a age function!";
+    //   }
+      
+    //   $myArr = array("car" => "Volvo", "age" => "18", "message" => "xyx");
+      
+    //   $myArr["age"]();
+
+    // array_push($color,"violet","green","parrot");
+    // var_dump($color);
+    //  array_splice($color,2,1);
+    // var_dump($color)
+
+    // $myArr+=["color"=>"red"];
+    // var_dump($myArr);
+
+    // array_splice($color,2,1);
+    // var_dump($color)
+
+    // $new = array_diff($myArr,[18,"xyx"]);
+    // print_r($new);
+
+    // unset($myArr["message"]);
+    // var_dump($myArr)
+
+    // $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+    // asort($age);
+    // ksort($age);
+    // arsort($age);  
+   
+    // krsort($age);  
+    // foreach($age as $x => $x_value) {
+    //     echo "Key=" . $x . ", Value=" . $x_value;
+    //     echo "<br>";}
+
+    // print_r(array_change_key_case($age,CASE_LOWER));
+
+    // $a = array(
+    //     array(
+    //       'id' => 5698,
+    //       'first_name' => 'Peter',
+    //       'last_name' => 'Griffin',
+    //     ),
+    //     array(
+    //       'id' => 4767,
+    //       'first_name' => 'Ben',
+    //       'last_name' => 'Smith',
+    //     ),
+    //     array(
+    //       'id' => 3809,
+    //       'first_name' => 'Joe',
+    //       'last_name' => 'Doe',
+    //     )
+    //   );
+    //   $firstname = array_column($a,'first_name');
+    //   print_r($firstname);
+    
+?>
+</pre>
+</body>
+</html>
