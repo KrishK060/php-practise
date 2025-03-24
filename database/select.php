8b036a99e337b6b3235ac7228adcf6d147610d27
Krishk060 <krishkalaria0601@gmail.com>
2025-03-24 13:59:01 +0530

    select.php

---
+++ database/select.php
@@ -0,0 +1,42 @@
+<?php
+// $servername = "localhost";
+// $username = "krish";
+// $password = "123";
+// $database = "demo"; 
+// $conn = new mysqli($servername, $username, $password, $database);
+
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+// $sql = "select id,firstname,lastname from myguests";
+// $result = $conn->query($sql);
+
+// if($result->num_rows>0){
+//     while($row = $result->fetch_assoc()){
+//         echo "id:".$row["id"]."Name:".$row["firstname"]." ".$row["lastname"]."\n";
+//     }
+// }else{
+//     echo "0 result found";
+// }
+$servername = "localhost";
+$username = "krish";
+$password = "123";
+$database = "demo"; 
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "select id,firstname,lastname from myguests";
+$result = $conn->query($sql);
+
+if($result->num_rows>0){
+    echo"<table><tr><th>ID</th><th>Name</th></td>";
+    while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</tr></td>";
+    }
+}else{
+    echo "0 result found";
+}
