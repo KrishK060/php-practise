29c7a249e26d0924773e0e223068f45206201433
Krishk060 <krishkalaria0601@gmail.com>
2025-03-24 14:11:41 +0530

    createa and implemented the orderby in sql with the help of php

---
+++ database/order.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "krish";
+$password = "123";
+$database = "demo"; 
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$sql = "select id,firstname,lastname from myguests order by lastname desc";
+$result = $conn->query($sql);
+
+if($result->num_rows>0){
+    echo"<table><tr><th>ID</th><th>Name</th></td>";
+    while($row = $result->fetch_assoc()){
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</tr></td>";
+    }
+}else{
+    echo "0 result found";
+}
