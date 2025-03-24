e9b17109c54de3e2c11ba21e22637f1494ff1f96
Krishk060 <krishkalaria0601@gmail.com>
2025-03-24 11:39:44 +0530

    database connected successfully

---
+++ database/db.php
@@ -0,0 +1,26 @@
+<?php 
+$servername = "localhost";
+$username = "krish";
+$password = "123";
+$database = "demo"; 
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// echo "Database connected successfully";
+$sql = "create table myguests(
+id int AUTO_INCREMENT PRIMARY KEY,
+firstname varchar(255) NOT NULL,
+lastname varchar(255) NOT NULL
+)";
+
+if($conn->query($sql) === TRUE){
+    echo "table created succesfully";
+}else{
+    echo "error".$conn->error;
+}
+
+?>
